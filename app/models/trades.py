0e1dfb41bbeb246827f68ad14a1bbc787de87276
Branly0 <nkasibranly01@gmail.com>
2026-02-16 16:38:00 +0100

    working on the trade object

--- app/models/trades.py
+++ app/models/trades.py
@@ -1,10 +1,16 @@
-from sqlalchemy import Column, Integer, Float
+from sqlalchemy import Column, Integer, Float, Boolean, String 
 from app.db.session import Base
 
 class Trade(Base):
     __tablename__ = "trades"
 
     id = Column(Integer, primary_key=True, index=True)
+    max_duration_min = Column(Integer, nullable=True)
     entry_price = Column(Float, nullable=False)
     stop_loss = Column(Float, nullable=False)
     take_profit = Column(Float, nullable=False)
+    is_opend = Column(Boolean, nullable=False, default=False)
+    time_frame = Column(Integer, nullable=False)
+    session = Column(String, nullable=False)
+    
+
