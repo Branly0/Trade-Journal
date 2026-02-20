fb1220fbee96094c3adbedaebeda85faf0eed8b3
Branly0 <nkasibranly01@gmail.com>
2026-02-20 10:47:49 -0800

    added some sepparate tables rady to relations and enums

--- app/models/trades.py
+++ app/models/trades.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Float, Boolean, String 
+from sqlalchemy import Uuid, Column, Integer, Float, Boolean, String 
 from app.db.session import Base
 
 class Trade(Base):
@@ -6,11 +6,34 @@ class Trade(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     max_duration_min = Column(Integer, nullable=True)
-    entry_price = Column(Float, nullable=False)
+    entry = Column(Float, nullable=False)
     stop_loss = Column(Float, nullable=False)
     take_profit = Column(Float, nullable=False)
-    is_opend = Column(Boolean, nullable=False, default=False)
+    is_closed = Column(Boolean, nullable=False, default=False)
     time_frame = Column(Integer, nullable=False)
     session = Column(String, nullable=False)
-    
+    strategy = Column(String, nullable=False) #is an enum in the frontend, but for simplicity, we can store it as a string in the database
+    symbol_id = Column(Integer, nullable=False) # Foreign key to symbols table, assuming you have a symbols table with an integer primary key
+    user_id = Column(Uuid, nullable=False) # Foreign key to users table, assuming you have a users table with a UUID primary key
 
+
+class ClosedTrade(Base):
+    __tablename__ = "closed_trades"
+
+    id = Column(Integer, primary_key=True, index=True)
+    exit = Column(Float, nullable=False)
+    trade_id = Column(Integer, nullable=False) # Foreign key to trades table
+
+class TradeNote(Base):
+    __tablename__ = "trade_notes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    note = Column(String, nullable=False)
+    trade_id = Column(Integer, nullable=False) # Foreign key to trades table
+
+class TradeScreenshot(Base):
+    __tablename__ = "trade_screenshots"
+
+    id = Column(Integer, primary_key=True, index=True)
+    screenshot_url = Column(String, nullable=False)
+    trade_id = Column(Integer, nullable=False) # Foreign key to trades table
---
+++ app/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Uuid, Column, Integer, Float, Boolean, String 
+from app.db.session import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Uuid, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    phone = Column(String, unique=True, nullable=True)
+    hashed_password = Column(String, nullable=False)
+    user_type = Column(String, nullable=False) #is an enum in the frontend, but for simplicity, we can store it as a string in the database
+    profile_picture_url = Column(String, nullable=True)
+    is_active = Column(Boolean, default=True)
